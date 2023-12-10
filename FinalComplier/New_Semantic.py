40076b8bfe7cc73075593b45d79e743d647de2ae
Fouz <Fouz6@outlook.com>
2023-12-11 02:28:47 +0500

    MDM error fixed

--- FinalComplier/New_Semantic.py
+++ FinalComplier/New_Semantic.py
@@ -137,6 +137,8 @@ class Semantic:
                 print(f"Type Mismatched {T1} and {T2}")
                 exit()
         if(OP in ["<", ">", "!", ">=", "<=", "==", "!="]):
+            if((T1 == "float" and T2 == "int") or (T2 == "float" and T1 == "int")):
+                return {"T":"bool"}
             if(T1 == T2 ):
                 return {"T":"bool"}
             else:
